97d0e973d99f8ec51399efb5c992ec599b04f5e9
Jamie Martines <dev.jpm@gmail.com>
2021-06-10 21:42:12 -0400

    Just realized there's git integration, pushing up as is

---
+++ src/components/Quiz.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { shuffleArray } from '../utils/utils';
+
+export const QuizWrapper = ({ quizzes }) => {
+  const [index, setIndex] = React.useState(0);
+  const [finished, setFinished] = React.useState(false);
+
+  const nextQuiz = () => {
+    console.log('next quiz');
+    if (index < quizzes.length - 1) {
+      setIndex((index) => index + 1);
+    } else {
+      console.log('maxed out on quizzes');
+      setFinished(true);
+    }
+  };
+
+  const reset = () => {
+    setIndex(0);
+    setFinished(false);
+  };
+
+  if (quizzes[index] !== undefined && !finished) {
+    return <Quiz quiz={quizzes[index]} nextQuiz={nextQuiz} />;
+  } else if (finished) {
+    console.log('no more quizzes');
+    return (
+      <>
+        <p>No more quizzes</p>
+        <button onClick={reset}>Try again?</button>
+      </>
+    );
+  }
+};
+
+const Quiz = ({ quiz, nextQuiz }) => {
+  const [index, setIndex] = React.useState(0);
+  const [answers, setAnswers] = React.useState([
+    ...quiz.questions[0].incorrectAnswers,
+    quiz.questions[0].correctAnswer,
+  ]);
+  const [correct, setCorrect] = React.useState(null);
+  const [numCorrect, setNumCorrect] = React.useState(0);
+  const [results, setResults] = React.useState(null);
+
+  React.useEffect(() => {
+    if (index === 0 && answers === null) {
+      setAnswers((answers) => shuffleArray(answers));
+    } else {
+      setAnswers([
+        ...quiz.questions[index].incorrectAnswers,
+        quiz.questions[index].correctAnswer,
+      ]);
+      setAnswers((answers) => shuffleArray(answers));
+    }
+  }, [index, quiz]);
+
+  const incrementIndex = () => {
+    setIndex((i) => (i === quiz.questions.length - 1 ? i : i + 1));
+    setCorrect(null);
+  };
+
+  const checkAnswer = (answer) => {
+    if (answer == quiz.questions[index].correctAnswer) {
+      console.log('correct');
+      setCorrect(true);
+      setNumCorrect((numCorrect) => numCorrect + 1);
+    } else {
+      console.warn('incorrect');
+      setCorrect(false);
+    }
+  };
+
+  const showResults = () => {
+    setResults(true);
+  };
+
+  if (!results && quiz !== undefined) {
+    return (
+      <>
+        <h1>{quiz.title}</h1>
+        <h4>{quiz.questions[index].text}</h4>
+        <ul>
+          {answers.map((answer, i) => {
+            return (
+              <li key={i} onClick={() => checkAnswer(answer)}>
+                {answer}
+              </li>
+            );
+          })}
+        </ul>
+        {correct && <div>Correct</div>}
+        {!correct && correct !== null && <div>Incorrect</div>}
+        <NextButtons
+          index={index}
+          increment={incrementIndex}
+          length={quiz.questions.length - 1}
+          results={showResults}
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Results</h1>
+        <h2>
+          <strong>{numCorrect}</strong> out of{' '}
+          <strong>{quiz.questions.length}</strong>
+        </h2>
+        <button
+          onClick={() => {
+            setNumCorrect(0);
+            setIndex(0);
+            setResults(null);
+            nextQuiz();
+          }}
+        >
+          Next Quiz
+        </button>
+      </>
+    );
+  }
+};
+
+const NextButtons = ({ index, increment, length, results }) => {
+  if (index === length) {
+    return <button onClick={results}>Show Results</button>;
+  } else {
+    return <button onClick={increment}>Next Question</button>;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getMoreQuizzes, getQuizzes } from './data/quizzes';
+import { QuizWrapper } from './components/Quiz.js';
 
 import './styles.css';
 
+const appReducer = (state, action) => {
+  if (action.type === 'success') {
+    return {
+      ...state,
+      quizzes: action.data,
+      error: null,
+    };
+  } else if (action.type === 'error') {
+    console.log(action.error.message);
+  } else {
+    throw new Error(`Unsupported action.type`);
+  }
+};
+
 const App = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [state, dispatch] = React.useReducer(appReducer, { error: null });
+  const fetchedQuizzes = React.useRef([]);
+
+  React.useEffect(() => {
+    if (fetchedQuizzes.current) {
+      setLoading(true);
+      Promise.all([getQuizzes(), getMoreQuizzes()]).then((data) => {
+        Array.prototype.push.apply(data[0], data[1]);
+        setLoading(false);
+        dispatch({ type: 'success', data: data[0] });
+      });
+    }
+  }, []);
+
   return (
     <div className="app">
-      <h1>Hello, World!</h1>
-      <p>Thanks so much for taking our frontend takehome assessment!</p>
-      <p>
-        See details in <code>./README.md</code> .
-      </p>
+      {loading && <p>Loading...</p>}
+
+      {state.error && <p className="error">{state.error}</p>}
+
+      {state.quizzes !== null && state.quizzes !== undefined ? (
+        <QuizWrapper quizzes={state.quizzes} />
+      ) : null}
     </div>
   );
 };
